750c17627bd66271c6c36682478d04bb447c393c
Zenia Gist <zeniagist@gmail.com>
2020-12-25 17:28:59 +0000

    added javascript file and linked to index.html

---
+++ index.js
@@ -0,0 +1,21 @@
+$(function(){
+  // declare variables
+
+  // on page load hide elements we don'ts need, leave only text area and start button
+
+  // Click on Start Reading
+
+  // Click on New
+
+  // Click on Pause
+
+  // Click on Resume
+
+  // Change Font Size
+  
+  // Change Speed
+
+  // Progress Slider
+
+  // functions
+})
